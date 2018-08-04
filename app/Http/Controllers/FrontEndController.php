325ffdd48ba259f42a8b29b5a87fa1c22098311e
sergioratti <sratti@hotmail.co.uk>
2018-08-04 10:59:22 +0200

    front end first,second and third postt

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Setting ;
+use App\Category ;
+use App\Post ;
 
 
 class FrontEndController extends Controller
@@ -11,6 +13,10 @@ class FrontEndController extends Controller
     public function index() {
         $setting = Setting::first() ;
 
-        return view('welcome')->with('setting', $setting) ;
+        return view('welcome')->with('setting', $setting)
+        ->with('categories', Category::take(4)->get())
+        ->with('first_post', Post::orderBy('created_at','desc')->first()) 
+        ->with('second_post', Post::orderBy('created_at','desc')->skip(1)->take(1)->get()->first()) 
+        ->with('third_post', Post::orderBy('created_at','desc')->skip(2)->take(1)->get()->first()) ;
     }
 }
